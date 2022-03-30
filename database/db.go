50c2c6a95c9b07481b0638bbe3cc3ea3e9ba7a92
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-03-30 15:16:00 +0700

    finish database

--- database/db.go
+++ database/db.go
@@ -2,12 +2,14 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 //mongodbInstance
@@ -27,5 +29,21 @@ func ConnectDB() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel() // cancel timeout
-	
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connect Successfully")
+	fmt.Sprintln("Connect Successfully")
+
+	MongoIns = MongoInstance{
+		Client: client,
+		Db : client.Database(os.Getenv("DATABASE_NAME")),
+	}
+
 }
